fd2d2a859a5e9f4602164d9f9c0fa4b16b26c06e
Rondinele Guimaraes <rondineleg@edu.univali.br>
2020-06-12 12:14:11 -0300

    Adicionando o datacontext

---
+++ src/ReservaSala.Api/Data/AppDataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReservaSala.Api.Models;
+
+namespace ReservaSala.Api.Data
+{
+    public class AppDataContext : DbContext
+    {
+        public AppDataContext (DbContextOptions<AppDataContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Aluno> Aluno { get; set; }
+        public DbSet<Sala> Sala { get; set; }
+        public DbSet<Bloco> Bloco { get; set; }
+        public DbSet<Reserva> Reserva { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+
+        
+    }
+}
