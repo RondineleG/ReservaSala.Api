33fd503a8179f7d7ed56e9256451d3ef7ba0972f
Rondinele Guimaraes <rondineleg@edu.univali.br>
2020-06-12 12:13:02 -0300

    Model Aluno

---
+++ src/ReservaSala.Api/Models/Aluno.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ReservaSala.Api.Models
+{
+    public class Aluno
+    {
+        public Aluno()
+        {
+            DataCadastro = DateTime.Now;
+        }
+        public int AlunoId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public string RA { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime DataCadastro { get; set; }
+
+    }
+}
