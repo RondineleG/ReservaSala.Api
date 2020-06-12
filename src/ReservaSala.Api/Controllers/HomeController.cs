d5985236f9cc447f59416690c993728c01e56e4e
Rondinele Guimaraes <rondineleg@edu.univali.br>
2020-06-12 12:30:50 -0300

    Adicionando controller home e alterando url padrao

---
+++ src/ReservaSala.Api/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReservaSala.Api.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public string Index()
+        {
+            return "API Running...";
+        }
+    }
+}
